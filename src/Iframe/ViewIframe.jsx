10a6ae2139784dd7e10109b718599c8b1fe4b6d3
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-02-04 18:12:46 +0200

    Remove logging

--- src/Iframe/ViewIframe.jsx
+++ src/Iframe/ViewIframe.jsx
@@ -26,7 +26,7 @@ const messages = defineMessages({
 
 const View = ({ data, intl }) => {
   const [visible, setVisibility] = useState(false);
-  console.log('map', data);
+  // console.log('map', data);
   // partialVisibility={true}
   return (
     <div
