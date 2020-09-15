d83f3f2aea4c3ed459f33f8ee55ab33964f2369a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-09-15 11:06:46 +0300

    Export convenience utils

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import { applyConfig } from './config';
 
+export addPrivacyProtectionToSchema from './PrivacyProtection';
+export PrivacyProtection from './PrivacyProtection/PrivacyProtection';
+
 export default applyConfig;
