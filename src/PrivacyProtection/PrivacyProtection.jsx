b02c8570f1b5e463715ac327ba1fba1011bd4f51
Daniela Mormocea <daniela.mormocea@gmail.com>
2021-10-12 19:27:42 +0300

    pretty

--- src/PrivacyProtection/PrivacyProtection.jsx
+++ src/PrivacyProtection/PrivacyProtection.jsx
@@ -134,7 +134,9 @@ export default injectIntl(
         fetch(
           `${getBaseUrl(
             '',
-          )}/cors-proxy/https://screenshot.eea.europa.eu/api/v1/retrieve_image_for_url?url=${encodeURIComponent(url)}&w=1920&waitfor=4000`,
+          )}/cors-proxy/https://screenshot.eea.europa.eu/api/v1/retrieve_image_for_url?url=${encodeURIComponent(
+            url,
+          )}&w=1920&waitfor=4000`,
         )
           .then((e) => e.blob())
           .then((blob) => {
