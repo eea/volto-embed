0686e19518dfb8e53a64300badb13887575b031a
nileshgulia1 <nileshgulia@gmail.com>
2021-10-12 16:12:35 +0530

    decreased wait time

--- src/PrivacyProtection/PrivacyProtection.jsx
+++ src/PrivacyProtection/PrivacyProtection.jsx
@@ -134,7 +134,7 @@ export default injectIntl(
         fetch(
           `${getBaseUrl(
             '',
-          )}/cors-proxy/https://screenshot.eea.europa.eu/api/v1/retrieve_image_for_url?url=${url}&w=1920&waitfor=6000${
+          )}/cors-proxy/https://screenshot.eea.europa.eu/api/v1/retrieve_image_for_url?url=${url}&w=1920&waitfor=4000${
             param ? `&waitforselector=img[src*=${param}]` : ''
           }`,
         )
