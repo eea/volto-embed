1dc059fa58f7a1789e4b570d1631226b404fa805
nileshgulia1 <nileshgulia@gmail.com>
2022-04-28 17:53:05 +0530

    fix regex

--- src/PrivacyProtection/PrivacyProtection.jsx
+++ src/PrivacyProtection/PrivacyProtection.jsx
@@ -72,7 +72,7 @@ const CookieWatcher = (domain_key, cookies, pollingRate = 250) => {
 const getFilteredURL = (url, connected_data_parameters = []) => {
   if (!connected_data_parameters?.length) return url;
   let decodedURL = decodeURIComponent(url);
-  const queries = decodedURL.match(/(<<)(.*?)*>>$/g); //safari: don't use lookbehind
+  const queries = decodedURL.match(/(<<)(.*?)*>>/g); //safari: don't use lookbehind
   if (!queries?.length) return url;
 
   const filteredQueries = queries.map((query) =>
