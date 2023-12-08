d2695617e3b0996a106d023649857196c6a802ca
Miu Razvan <miu.razvan28@gmail.com>
2023-12-08 17:50:35 +0200

    move url widgets to volto-object-widget

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import installBlocks from './Blocks';
 import MapView from './Views/MapView';
 import MapsViewWidget from './Widgets/MapsViewWidget';
 import MapsWidget from './Widgets/MapsWidget';
-import UrlWidget from './Widgets/UrlWidget';
-import InternalUrlWidget from './Widgets/InternalUrlWidget';
 export {
   PrivacyProtection,
   addPrivacyProtectionToSchema,
@@ -13,9 +11,6 @@ export default function applyConfig(config) {
   config.views.contentTypesViews.map = MapView;
   config.widgets.id.maps = MapsWidget;
   config.widgets.views.id.maps = MapsViewWidget;
-
-  config.widgets.widget.url = UrlWidget;
-  config.widgets.widget.internal_url = InternalUrlWidget;
 
   config.settings.allowed_cors_destinations = [
     ...config.settings.allowed_cors_destinations,
