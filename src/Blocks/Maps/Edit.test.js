ece8acd585c09e52901afc6a2b5ad03ef91b1804
eea-jenkins <eea-github@googlegroups.com>
2024-03-18 15:03:37 +0100

    style: Automated code fix

--- src/Blocks/Maps/Edit.test.js
+++ src/Blocks/Maps/Edit.test.js
@@ -26,8 +26,7 @@ describe('Test Maps Block editing', () => {
 
   const data = {
     '@type': 'maps',
-    url:
-      '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11255.043736345397!2d24.6862147!3d45.15143895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474d3248f30cd7b5%3A0x307c5acf21ded9e3!2sHotel%20Posada!5e0!3m2!1sro!2sro!4v1701254795494!5m2!1sro!2sro" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+    url: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11255.043736345397!2d24.6862147!3d45.15143895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474d3248f30cd7b5%3A0x307c5acf21ded9e3!2sHotel%20Posada!5e0!3m2!1sro!2sro!4v1701254795494!5m2!1sro!2sro" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
     dataprotection: {
       enabled: false,
       privacy_statement: 'This is a privacy statement',
